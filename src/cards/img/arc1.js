5133fe2e2ed30372950f25b075f8e45890f50f57
feindreh <119032218+feindreh@users.noreply.github.com>
2023-02-23 16:52:41 +0100

    add card creation

---
+++ src/cards/img/arc1.js
@@ -0,0 +1,26 @@
+import duo from "./duo.jpeg"
+import gohan from "./gohan.jpeg"
+import goku from "./goku.jpeg"
+import piccolo from "./piccolo.webp"
+import raditz from "./raditz.webp"
+
+
+const card = (name,logo) => {
+    return {name,logo}
+}
+
+
+
+const arc1 = () => {
+    const arr = [];
+
+    arr.push(card("Piccolo & Goku",duo))
+    arr.push(card("Gohan",gohan))
+    arr.push(card("Goku",goku))
+    arr.push(card("Piccolo",piccolo))
+    arr.push(card("Raditz",raditz))
+
+    return arr
+}
+
+export default arc1
