3e9659d46695357a0ccbddd049c5c11bbde752e1
feindreh <119032218+feindreh@users.noreply.github.com>
2023-02-23 16:53:33 +0100

    add basic card layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import Memory from "./components/memory";
 import Header from "./components/header";
+import arc1 from "./cards/img/arc1";
+import "./App.css"
 
 function App() {
+
+
   return (
   <div>
       <Header/>
-      <Memory/>
+      <Memory cards={arc1()}/>
   </div>
   );
 }
