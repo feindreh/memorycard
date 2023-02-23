63286d9a7710aa0c786fbd14a21c62439ea6af3e
feindreh <119032218+feindreh@users.noreply.github.com>
2023-02-24 00:40:40 +0100

    add cool score display

--- src/components/header.js
+++ src/components/header.js
@@ -2,11 +2,19 @@ function Header(props){
 
     const {score,scoreMax} = props
 
+    const arr = []
+    for(let i = 1; i<= scoreMax; i++){
+        if(i <= score){arr.push((<div className = "score full"></div>))}else{
+            arr.push((<div className = "score"></div>))
+        }
+    }
+
 
     return(
         <div id="header">
-            <div id="score">{score}/{scoreMax}</div>
+            
             <h1>Every Image One Time</h1>
+            <div id="score">{arr}</div>
         </div>
     )
 }
