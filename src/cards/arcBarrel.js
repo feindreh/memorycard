bff6f96526407615abe171ec7816ec049de0ffb3
feindreh <119032218+feindreh@users.noreply.github.com>
2023-02-23 23:46:16 +0100

    add arc2

--- src/cards/arcBarrel.js
+++ src/cards/arcBarrel.js
@@ -1,10 +1,12 @@
 import arc0 from "./arc0/arc0";
 import arc1 from "./arc1/arc1";
+import arc2 from "./arc2/arc2";
 
 
 const CARDS = []
 
 CARDS.push(arc0)
 CARDS.push(arc1)
+CARDS.push(arc2)
 
 export default CARDS
