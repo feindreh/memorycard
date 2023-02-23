b9a0d443f7c39dff4e8d80750b1c45d763a736b1
feindreh <119032218+feindreh@users.noreply.github.com>
2023-02-23 16:58:08 +0100

    add score and highscore

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import Memory from "./components/memory";
 import Header from "./components/header";
 import arc1 from "./cards/img/arc1";
 import "./App.css"
+import { useState } from "react";
 
 function App() {
+
+  const [score,setScore] = useState(0)
+  const [highScore,setHighScore] = useState(0)
 
 
   return (
   <div>
-      <Header/>
+      <Header score={score} highScore={highScore}/>
       <Memory cards={arc1()}/>
   </div>
   );
